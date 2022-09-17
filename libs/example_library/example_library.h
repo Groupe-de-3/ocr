b9ee4785eedffdfece973923588696d90581944e
Heavenston <malolegendrelemaire@@gmail.com>
2022-09-17 15:02:27 +0200

    Added comments to the add function

--- libs/example_library/example_library.h
+++ libs/example_library/example_library.h
@@ -1,3 +1,9 @@
 #pragma once
 
+/**
+ * Adds the two given numbers
+ * @param int a The first number
+ * @param int b The second number
+ * @return The result of the addition
+ */
 int add(int a, int b);
