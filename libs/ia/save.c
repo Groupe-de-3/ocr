2440a3875d88edc454633b8cce3857f07e7f747d
Heavenston <malolegendrelemaire@@gmail.com>
2022-10-06 07:34:17 +0200

    Cleaned up ia lib

--- libs/ia/save.c
+++ libs/ia/save.c
@@ -1,58 +1,23 @@
+#include "save.h"
+
+#include <err.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int    layers_number  = 3; // number of idden layer
+int    layers_number  = 3; // number of hidden layer
 int    layers_sizes[] = {2, 5, 1};
 double inputs[]       = {1, 0};
 
-typedef struct Neurone {
-    double biais;
-    double weights[10];
-
-} N;
-
-typedef struct Layer {
-    N layer_[10];
-} Layer;
-
-typedef struct neural_network {
-    Layer layers_[10];
-} neural_network;
-
-double str_to_double(char *chaine) {
-    double res = chaine[0] - 48;
-    int    i   = 2;
-    while (chaine[i] != '\0') {
-        res += (chaine[i] - 48) / (pow(10, i - 1));
-        i++;
-    }
-
-    return res;
-}
-
-int str_to_int(char *chaine) {
-    int res = chaine[0] - 48;
-    int i   = 2;
-    while (chaine[i] != '\0') {
-        res += (chaine[i] - 48) / (pow(10, i - 1));
-        i++;
-    }
-
-    return res;
-}
-
 // TRAITEMENT FICHIER / neural network
 
-void clear() // remove neural_network data
-{
-    system("rm neural_network.txt");
+void clear() {
+    if (remove("neural_network.txt") != 0)
+        errx(1, "Could not delete file neural_network.txt");
 }
 
-void save(neural_network NN
-) // save the neural network in the file "neural_network"
-{
+void save(neural_network NN) {
     clear();
     FILE *fichier = NULL;
     fichier       = fopen("neural_network.txt", "w");
@@ -67,7 +32,7 @@ void save(neural_network NN
         for (int weight_ind = 0; weight_ind < layers_sizes[layer_ind];
              weight_ind++) {
             fprintf(
-                fichier, "%f\n", NN.layers_[layer_ind].layer_[weight_ind].biais
+                fichier, "%f\n", NN.layers_[layer_ind].layer_[weight_ind].bias
             );
             if (layer_ind < layers_number - 1) {
                 for (int j = 0; j < layers_sizes[layer_ind + 1]; j++)
@@ -82,8 +47,7 @@ void save(neural_network NN
     fclose(fichier);
 }
 
-neural_network load() // initialisation of the neural network from a file
-{
+neural_network load() {
     neural_network NN;
 
     FILE *fichier = NULL;
@@ -92,11 +56,11 @@ neural_network load() // initialisation of the neural network from a file
     // ini base
     char chaine[10] = "";
     fgets(chaine, 10, fichier);
-    layers_number = str_to_int(chaine);
+    layers_number = atoi(chaine);
 
     for (int i = 0; i < layers_number; i++) {
         fgets(chaine, 10, fichier);
-        layers_sizes[i] = str_to_int(chaine);
+        layers_sizes[i] = atoi(chaine);
     }
 
     for (int layer_ind = 0; layer_ind < layers_number; layer_ind++) {
@@ -105,15 +69,15 @@ neural_network load() // initialisation of the neural network from a file
             //create neural && read neural
 
             N    new_neurone;
-            char chaine[10] = "";
+            char chaine2[10] = "";
 
-            fgets(chaine, 10, fichier); // biais
-            new_neurone.biais = str_to_double(chaine);
+            fgets(chaine2, 10, fichier); // biais
+            new_neurone.bias = atof(chaine2);
 
             for (int j = 0; j < layers_sizes[layer_ind + 1]; j++) // weights
             {
-                fgets(chaine, 10, fichier);
-                double res             = str_to_double(chaine);
+                fgets(chaine2, 10, fichier);
+                double res             = atof(chaine2);
                 new_neurone.weights[j] = res;
             }
 
@@ -130,8 +94,7 @@ neural_network load() // initialisation of the neural network from a file
     return NN;
 }
 
-neural_network init() // initialisation of a new neural network
-{
+neural_network init() {
     neural_network NN;
 
     for (int layer_ind = 0; layer_ind < layers_number; layer_ind++) {
@@ -140,9 +103,9 @@ neural_network init() // initialisation of a new neural network
             N new_neurone; //create neural
 
             if (layer_ind == 0)
-                new_neurone.biais = inputs[i];
+                new_neurone.bias = inputs[i];
             else
-                new_neurone.biais = (rand() / ((double)RAND_MAX)); // ini biais
+                new_neurone.bias = (rand() / ((double)RAND_MAX)); // ini biais
 
             if (i < layers_number - 1) // ini weights
             {
--- libs/ia/save.h
+++ libs/ia/save.h
@@ -1,5 +1,32 @@
 #pragma once
 
-neural_network load();
-neural_network init();
-void           save(neural_network NN);
+typedef struct Neurone {
+    double bias;
+    double weights[10];
+} N;
+
+typedef struct Layer {
+    N layer_[10];
+} Layer;
+
+typedef struct neural_network {
+    Layer layers_[10];
+} neural_network;
+
+/*! \brief Initialize a new neural network with random weights and biases.
+ */
+neural_network init(void);
+
+/*! \brief Loads the neural network saved in "neural_network.txt"
+ */
+neural_network load(void);
+/*! \brief Serialize a neural network into a file named "neural_network.txt".
+ *
+ *  The file can be removed with #clear
+ *
+ *  \param NN The neural network.
+ */
+void save(neural_network NN);
+/*! \brief Deletes files created by #save
+ */
+void clear(void);
