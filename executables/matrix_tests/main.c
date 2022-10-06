3434212260c75a9ff0f53f56e2f75e31bc14cbf0
Heavenston <malolegendrelemaire@@gmail.com>
2022-10-06 07:17:14 +0200

    Wouaw

--- executables/matrix_tests/main.c
+++ executables/matrix_tests/main.c
@@ -6,29 +6,35 @@
 #define VALUE 5
 
 int main() {
-    Matrix(int) id = m_new(int, 2, 2);
-    m_copy(id, (int[]){1, 0, 0, 1});
+    Matrix(int) id = m_new(int, 1, 2);
+    m_copy(id, (int[]){
+        1, 1,
+        3, 1,
+    });
 
-    Matrix(int) m = m_new(int, 2, 2);
-    m_copy(
-        m,
-        (int[]){
-            22,
-            10,
-            7,
-            10,
-        }
-    );
+    Matrix(int) m = m_new(int, 2, 1);
+    m_copy(m, (int[]){
+        22, 43,
+        7,   3,
+    });
 
-    Matrix(int) out = m_new(int, 1, 2);
+    Matrix(int) out = m_new(int, 2, 2);
     m_mul(id, m, out);
 
+    for (size_t x = 0; x < m_width(out); x++)
+        printf("------");
+    printf("-\n");
     for (size_t y = 0; y < m_height(out); y++) {
+        printf("|");
         for (size_t x = 0; x < m_width(out); x++) {
-            printf("%i ", m_get2(out, x, y));
+            printf(" %3i ", m_get2(out, x, y));
+            printf("|");
         }
         printf("\n");
     }
+    for (size_t x = 0; x < m_width(out); x++)
+        printf("------");
+    printf("-\n");
 
     m_destroy(id);
     m_destroy(m);
