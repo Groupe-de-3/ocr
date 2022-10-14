93a6d1b340207a15b9a7dfb7b5bd23a53b508ddd
Heavenston <malolegendrelemaire@@gmail.com>
2022-10-14 19:24:03 +0200

    Formated

--- libs/matrices/tests/tests.c
+++ libs/matrices/tests/tests.c
@@ -1,24 +1,33 @@
-#include <stdio.h>
 #include "tests.h"
+
+#include <stdio.h>
+
 #include "matrices.h"
 
-const char *test_functions[] = {
-    "test_add",
-    NULL
-};
+const char *test_functions[] = {"test_add", NULL};
 
 void test_add(void) {
     Matrix(int) id = m_new(int, 1, 2);
-    m_copy(id, (int[]){
-        1, 1,
-        3, 1,
-    });
+    m_copy(
+        id,
+        (int[]){
+            1,
+            1,
+            3,
+            1,
+        }
+    );
 
     Matrix(int) m = m_new(int, 2, 1);
-    m_copy(m, (int[]){
-        22, 43,
-        7, 3,
-    });
+    m_copy(
+        m,
+        (int[]){
+            22,
+            43,
+            7,
+            3,
+        }
+    );
 
     Matrix(int) out = m_new(int, 2, 2);
     m_mul(id, m, out);
--- tests/runner/main.c
+++ tests/runner/main.c
@@ -1,12 +1,12 @@
 #include <assert.h>
 #include <dlfcn.h>
 #include <err.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/wait.h>
-#include <fcntl.h>
 #include <string.h>
-#include <errno.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define xstr(s) str(s)
@@ -17,13 +17,13 @@ typedef void (*TestFunction)(void);
 
 /*! \brief Prints to stdout everything available without blocking from the
  *  given file descriptor.
- * 
+ *
  *  \param df The file descriptor to read from.
  */
 static void print_fd(int fd) {
     fcntl(fd, F_SETFL, O_NONBLOCK);
     while (1) {
-        char output[128];
+        char    output[128];
         ssize_t read_size = read(fd, output, sizeof(output));
         if (read_size <= 0)
             break;
@@ -33,7 +33,7 @@ static void print_fd(int fd) {
 }
 
 /*! \brief Executes the given function into a subprocess.
- *  
+ *
  *  \param[in]  test_function Function that will be run un a subprocess.
  *  \param[out] output_fd If not NULL, set to a file descriptor of the output
  *                         of the subprocess. (To be printed with #print_df)
@@ -46,16 +46,16 @@ static int execute_test(TestFunction test_function, int *output_fd) {
     error = pipe(out_pipe);
     if (error == -1)
         errx(1, "Could not create pipe: %s", strerror(errno));
-    
+
     pid_t pid = fork();
     if (pid == 0) {
-        // Captures the fork's stdout (0) and stderr (1) 
+        // Captures the fork's stdout (0) and stderr (1)
         // into the write end of the pipe.
         dup2(out_pipe[1], 0);
         dup2(out_pipe[1], 1);
 
         test_function();
-        
+
         // Be sure the outputs have been flushed
         fflush(stdout);
         fflush(stderr);
@@ -98,7 +98,7 @@ int execute_tests(const char *path) {
         TestFunction tst_fun = (TestFunction)dlsym(dl_handle, *counter);
         if ((error = dlerror()) != NULL)
             errx(1, "Invalid test lib file: %s", error);
-        
+
         int rs_fd;
         if (execute_test(tst_fun, &rs_fd) != 0) {
             result = 1;
@@ -112,7 +112,7 @@ int execute_tests(const char *path) {
         }
         close(rs_fd);
     }
-    
+
     return result;
 }
 
