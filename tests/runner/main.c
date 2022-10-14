23694a087c7068bc462f64a1920e29ec21349d7e
Heavenston <malolegendrelemaire@@gmail.com>
2022-10-14 02:14:21 +0200

    tests are now *preti*

--- tests/runner/main.c
+++ tests/runner/main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <dlfcn.h>
 #include <err.h>
 #include <stdio.h>
@@ -15,13 +16,12 @@ int execute_test(TestFunction fn) {
     pid_t pid = fork();
     if (pid == 0) {
         fn();
-
         exit(0);
     }
     else if (pid == -1) {
         errx(1, "Could not fork process");
     }
-    // If got to this point,this is the original process
+    // If got to this point, this is the parent process.
 
     int status = -1;
     waitpid(pid, &status, 0);
@@ -35,30 +35,38 @@ int execute_tests(const char *path) {
 
         void *dl_handle = dlopen(path, RTLD_LAZY);
         if (dl_handle == NULL)
-            errx(1, "%s\n", dlerror());
+            errx(1, "Invalid test lib file: %s\n", dlerror());
         dlerror();
 
+        // Loading the array of test functions (asumed valid).
         char **test_functions = dlsym(dl_handle, "test_functions");
         if ((error = dlerror()) != NULL)
-            errx(1, "%s", error);
+            errx(1, "Invalid test lib file: %s", error);
 
+        int result = 0;
+        // Iteration through the array.
         for (char **counter = test_functions; *counter != 0; counter++) {
-            printf("Running %s\n", *counter);
+            printf("  '%s' > ", *counter);
+            fflush(stdout);
 
             TestFunction tst_fun = (TestFunction)dlsym(dl_handle, *counter);
             if ((error = dlerror()) != NULL)
-                errx(1, "%s", error);
-            int result;
-            if ((result = execute_test(tst_fun)) != 0)
-                exit(result);
+                errx(1, "Invalid test lib file: %s", error);
+            if (execute_test(tst_fun) != 0) {
+                result = 1;
+                printf("Failed\n");
+            }
+            else {
+                printf("Passed!\n");
+            }
         }
 
-        exit(0);
+        exit(result);
     }
     else if (pid == -1) {
         errx(1, "Could not fork process");
     }
-    // If got to this point,this is the original process
+    // If got to this point,this is the original process.
 
     int status = -1;
     waitpid(pid, &status, 0);
@@ -66,21 +74,27 @@ int execute_tests(const char *path) {
 }
 
 int main() {
-    char        lib[50];
+    // Buffer for each test file names. (with i as counter into it)
+    char        lib[100];
     int         i    = 0;
     const char *text = values;
+
+    // Iterating through each space-separated list of test files.
     while (*text != '\0') {
         i = 0;
-        for (; *text != ' ' && *text != '\0'; text++)
+        // Copy the current word until eol or a space is hit.
+        for (; *text != ' ' && *text != '\0' && i < 100; text++)
             lib[i++] = *text;
+        assert(i < 100); // Assert not any file was larget than 100 characters
         if (*text == ' ')
             text++;
         lib[i] = 0;
 
         printf("Running tests for %s\n", lib);
         int result = execute_tests(lib);
-        if (result != 0) {
+        if (result != 0)
             printf("Test failed\n");
-        }
+        else
+            printf("Success !\n");
     }
 }
