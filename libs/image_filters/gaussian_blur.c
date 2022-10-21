1d9b12fa1dc5779035436631a074f5d55ef768e6
Heavenston <malolegendrelemaire@@gmail.com>
2022-10-21 20:46:18 +0200

    Added comments

--- libs/image_filters/gaussian_blur.c
+++ libs/image_filters/gaussian_blur.c
@@ -9,7 +9,8 @@
 
 size_t gaussian_blur_optimal_kernel_size(float param) {
     size_t size = (size_t)ceil(6.f * param);
-    return size + (1 - size & 0x1); // Make sure we return an odd value.
+    // Make sure we return an odd value.
+    return size + (1 - size & 0x1);
 }
 void gaussian_blur_populate_kernel(float *kernel_mat, float param) {
     float factor = 1.f / (2.f * M_PIf * param * param);
@@ -18,6 +19,8 @@ void gaussian_blur_populate_kernel(float *kernel_mat, float param) {
     size_t haf_w = w / 2;
     size_t h     = m_height(kernel_mat);
     size_t haf_h = h / 2;
+    
+    // Make sure the dimensions are odd
     assert((w & 0x1) == 1 && (h & 0x1) == 1);
 
     float sum = 0.;
@@ -32,6 +35,7 @@ void gaussian_blur_populate_kernel(float *kernel_mat, float param) {
         }
     }
 
+    // Normalize the output
     for (size_t y = 0; y < h; y++)
         for (size_t x = 0; x < w; x++)
             m_get2(kernel_mat, x, y) /= sum;
