5bb79d7e6a7541a63ae4688e1957f7c033671668
Heavenston <malolegendrelemaire@@gmail.com>
2022-10-23 03:26:50 +0200

    Print timings in image tests

--- executables/images_tests/main.c
+++ executables/images_tests/main.c
@@ -1,6 +1,8 @@
 #include <assert.h>
 #include <err.h>
 #include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
 
 #include "box_blur.h"
 #include "gaussian_blur.h"
@@ -10,7 +12,7 @@
 #include "matrices.h"
 #include "sobel_operator.h"
 
-void print_m(float *m) {
+static void print_m(float *m) {
     for (size_t y = 0; y < m_height(m); y++) {
         for (size_t x = 0; x < m_width(m); x++) {
             printf("%03.4f", m_get2(m, x, y));
@@ -18,6 +20,12 @@ void print_m(float *m) {
         }
         printf("\n");
     }
+}
+
+static long timediff(struct timeval start) {
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    return (now.tv_sec - start.tv_sec) * 1000 + now.tv_usec / 1000 - start.tv_usec / 1000;
 }
 
 int main(int argc, char **argv) {
@@ -28,8 +36,11 @@ int main(int argc, char **argv) {
             argv[0]
         );
     }
+    
+    struct timeval start;
 
-    printf("Reading file %s\n", argv[1]);
+    gettimeofday(&start, NULL);
+    printf("Reading file '%s'\n", argv[1]);
     Image           image;
     ImageLoadResult rslt =
         img_load_file(argv[1], &image, false, PixelFormat_Rgbf);
@@ -38,9 +49,13 @@ int main(int argc, char **argv) {
     }
     ImageView image_view    = imgv_default(&image);
     image_view.wraping_mode = WrappingMode_Clamp;
+    
+    printf("    Loaded image of size %zu x %zu\n", image.width, image.height);
 
+    printf("    Saving input to in.bmp\n");
     // Save input
     bmp_save_to_path("in.bmp", &image);
+    printf("    Done (%ldms)\n", timediff(start));
 
     if (false) {
         // Bluring image using box blur
@@ -60,6 +75,9 @@ int main(int argc, char **argv) {
         img_destroy(box_blur);
     }
 
+    gettimeofday(&start, NULL);
+    printf("Bluring image\n");
+
     // Bluring image
     Image blured = img_new(image.width, image.height, image.format);
     imgv_default(&blured);
@@ -68,19 +86,23 @@ int main(int argc, char **argv) {
     blur_view.wraping_mode = WrappingMode_Clamp;
 
     // Executing blur
-    printf("Bluring image\n");
     gaussian_blur_run(&image_view, &blur_view, 2.f);
 
+    printf("    Saving blur to blured.bmp\n");
     // Saving blur
     bmp_save_to_path("blured.bmp", &blured);
+    printf("    Done (%ldms)\n", timediff(start));
 
+    gettimeofday(&start, NULL);
     printf("Computing gradient\n");
     // Computing image's gradient
     Image     gradient      = img_new(image.width, image.height, image.format);
     ImageView gradient_view = imgv_default(&gradient);
 
     sobel_execute(&blur_view, &gradient_view, NULL);
+    printf("    Saving gradient to gradient.bmp\n");
     bmp_save_to_path("gradient.bmp", &gradient);
+    printf("    Done (%ldms)\n", timediff(start));
 
     img_destroy(image);
     img_destroy(blured);
