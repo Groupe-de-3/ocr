a33a1077ac20b8191ac99f9cca4a9ad2f0711f42
Romain DUBOIS <romain1.dubois@epita.fr>
2022-11-05 20:16:07 +0100

    solver

--- libs/sudoku/sudoku.c
+++ libs/sudoku/sudoku.c
@@ -38,86 +38,73 @@ bool contains(char *list, int x)
 }
 
 
-bool check_lines(char *board)
+/*bool check_lines(char *board)
 {
     for (int i = 0; i < 9; i++)
     {
         char *list = vec_new(char);
         for (int j = 0; j < 9; ++j)
         {
-            if (board[i * 9 + j] == EMPTY_CELL)
+            if (board[i * 9 + j] != EMPTY_CELL)
             {
-                continue;
+                if (contains(list, board[i * 9 + j]))
+                    return false;
+                *vec_push(&list, char) =  board[i * 9 + j];
+
             }
-            if (contains(list, board[i * 9 + j]))
-            {
-                vec_destroy(list);
-                return false;
-            }
-            *vec_push(&list, char) =  board[i * 9 + j];
         }
-        vec_destroy(list);
     }
     return true;
 }
 
-bool check_columns(char *board)
-{
+bool check_columns(char *board) {
     for (int i = 0; i < 9; i++)
     {
         char *list = vec_new(char);
-        for (int j = 0; j < 9; ++j)
+        for (int j = 0; j < 9; j++)
         {
-            if (board[j * 9 + i] == EMPTY_CELL)
+            if (board[j * 9 + i] != EMPTY_CELL)
             {
-                continue;
+                if (contains(list, board[j * 9 + i]))
+                    return false;
+                *vec_push(&list, char) = board[j * 9 + i];
             }
-            if (contains(list, board[j * 9 + i]))
-            {
-                vec_destroy(list);
-                return false;
-            }
-            *vec_push(&list, char) =  board[j * 9 + i];
         }
-        vec_destroy(list);
     }
     return true;
 }
 
-bool check_squares(char *board, int x, int y)
+
+
+bool check_squares(char *board)
 {
-    char *list = vec_new(char);
-    for (int i = 0; i < 3; ++i)
+    for (int i = 0; i < 9; i += 3)
     {
-        for (int j = 0; j < 3; ++j)
+        for(int j = 0; j < 9; j += 3)
         {
-            if (board[(i + x)* 9 + j + y] == EMPTY_CELL)
+            char *list = vec_new(char);
+            for(int x = 0; x < 3; x++)
             {
-                continue;
+                for(int y = 0; y < 3; y++)
+                {
+                    if (board[x * 9 + y] != EMPTY_CELL)
+                    {
+                        if (contains(list, board[x * 9 + y]))
+                            return false;
+                        *vec_push(&list, char) =  board[x * 9 + y];
+                    }
+                }
             }
-            if (contains(list, board[(i + x)*9 + j + y]))
-            {
-                vec_destroy(list);
-                return false;
-            }
-            *vec_push(&list, char) =  board[(i+x)*9 + j + y];
+
         }
     }
-    vec_destroy(list);
+
     return true;
 }
 
 bool is_board_valid(char *board)
 {
-    bool b = true;
-    for (int i = 0; i < 9; i += 3)
-    {
-        for (int j = 0; j < 9; j += 3)
-        {
-            b = b && check_squares(board, i, j);
-        }
-    }
-    return check_columns(board) && check_lines(board) && b;
+    return check_squares(board) && check_columns(board) && check_lines(board);
 }
 
 bool is_solved(char *board)
@@ -130,48 +117,130 @@ bool is_solved(char *board)
             {
                 return false;
             }
+            if (check_squares(board) && check_lines(board) && check_columns(board))
+                continue;
+            return false;
         }
     }
-    return is_board_valid(board);
-}
-
-bool __sudoku_solve(char *board, int i, int j)
-{
-    while (board[i*9+j] != EMPTY_CELL)
-    {
-        j+=1;
-        if(j >= 9)
-        {
-            i++;
-            j = 0;
-        }
-        if(i >= 9)
-        {
-            return is_solved(board);
-        }
-    }
-    for (char k = 1; k <= 9 ; ++k)
-    {
-
-        board[i * 9 + j] = k;
-        if(is_board_valid(board))
-        {
-            if(__sudoku_solve(board,i,j))
-            {
-                return true;
-            }
-        }
-    }
-    board[i*9+j] = EMPTY_CELL;
-    return is_solved(board);
+    return true;
 }
 
 
 bool sudoku_solve(char *board)
 {
-    int i = 0;
-    int j = 0;
-    return __sudoku_solve(board,i,j);
+    for(int i = 0; i < 9; i++)
+    {
+        for(int j = 0; j < 9; j++)
+        {
+            if (board[i * 9 + j] == EMPTY_CELL)
+            {
+                for (char num = 1; num < 10; num++)
+                {
+                    board[i * 9 + j] = num;
+                    if (is_board_valid(board))
+                        if(sudoku_solve(board))
+                            return true;
+                    board[i * 9 + j] = EMPTY_CELL;
+                }
+                if (board[i * 9 + j] == EMPTY_CELL)
+                    return false;
+            }
+        }
+    }
+    return is_solved(board);
 }
 
+*/
 
+bool checkBoardInterity (bool mustBeValid, char* board)
+{
+    for (int blockI = 0; blockI < 3; ++blockI) {
+        for (int blockJ = 0; blockJ < 3; ++blockJ)
+        {
+            for (int i = 0; i < 3; ++i)
+            {unsigned int foundBitMap = 0;
+                for (int j = 0; j < 3; ++j)
+                {
+                    unsigned int bit =
+                        1u << board[(blockI + i) * 3 + (blockJ + j) * 3];
+                    if ((foundBitMap & bit) != 0 && bit != 1)
+                        return false;
+                    foundBitMap |= bit;
+                }
+                if (mustBeValid && foundBitMap != 0x3FE)
+                    return false;
+            }
+        }}
+
+    for (int i = 0; i < 9; i++)
+    {
+        unsigned int foundBitMap = 0;
+        for (int j = 0; j < 9; j++)
+        {
+            unsigned int bit = 1u << board[i * 9 + j];
+            if ((foundBitMap & bit) != 0 && bit != 1)
+                return false;
+            foundBitMap |= bit;
+        }
+        if (mustBeValid && foundBitMap != 0x3FE)
+            return false;
+    }
+    for (int j = 0; j < 9; j++)
+    {
+        unsigned int foundBitMap = 0;
+        for (int i = 0; i < 9; i++)
+        {
+            unsigned int bit = 1u << board[j * 9 + i];
+            if ((foundBitMap & bit) != 0 && bit != 1)
+                return false;
+            foundBitMap |= bit;
+        }
+        if (mustBeValid && foundBitMap != 0x3FE)
+            return false;
+    }
+
+    return true;
+}
+
+bool IsBoardValid(char* board)
+{
+    return checkBoardInterity(false,board);
+}
+
+bool IsSolved(char *board)
+{
+    return checkBoardInterity(true,board);
+}
+
+bool Solve(int i, int j,char* board)
+{
+    while (j < 9 && board[i*9+j] != 0)
+    {
+        i++;
+        if(i >= 9)
+        {
+            i = 0;
+            j+=1;
+        }
+    }
+
+    if(j >= 9)
+    {
+        return IsSolved(board);
+    }
+    for (char k = 1; k <= 9; ++k)
+    {
+        board[i*9+j] = k;
+        if(!IsBoardValid(board))
+        {
+            continue ;
+        }
+        sudoku_print(board);
+        if(Solve(i,j,board))
+        {
+            return true;
+        }
+    }
+    board[i*9+j] = 0;
+    return false;
+}
--- libs/sudoku/sudoku.h
+++ libs/sudoku/sudoku.h
@@ -3,5 +3,7 @@
 #define EMPTY_CELL 0
 
 void sudoku_print(char *board);
-
-bool sudoku_solve(char *board);
+bool checkBoardInterity (bool mustBeValid, char* board);
+bool IsBoardValid(char* board);
+bool IsSolved(char *board);
+bool Solve(int i, int j,char* board);
